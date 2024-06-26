7c2b8e263598f5f1a882e118f7b54f5775eef68f
bobokrut <egor-evlampiev@mail.ru>
2024-06-27 01:49:38 +0200

    feat: added support for dynamic data sources for grafana

--- grafana/setup.sh
+++ grafana/setup.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
 wget $URL_CONFIG -O /grafana_dashboards/dashboard.json
+cat /grafana_dashboards/dashboard.json | yq ".datasources" -y > /etc/grafana/provisioning/datasources/datasource.yaml
+cat /grafana_dashboards/dashboard.json | yq ".dashboards" > /grafana_dashboards/dashboard.json
 echo "Starting Grafana"
 /run.sh
