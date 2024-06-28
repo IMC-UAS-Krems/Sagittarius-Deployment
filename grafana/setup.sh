02fd2b18c2022c51f2793cf0018f28ee80337aa7
bobokrut <egor-evlampiev@mail.ru>
2024-06-28 02:17:14 +0200

    fixed `yq` queries

--- grafana/setup.sh
+++ grafana/setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 wget $URL_CONFIG -O /grafana_dashboards/dashboard.json
-cat /grafana_dashboards/dashboard.json | yq -o yaml ".datasources" > /etc/grafana/provisioning/datasources/datasource.yaml
-cat /grafana_dashboards/dashboard.json | yq -o json ".dashboards"  > /grafana_dashboards/dashboard.json
+cat /grafana_dashboards/dashboard.json | yq -P ".datasources" > /etc/grafana/provisioning/datasources/datasource.yaml
+cat /grafana_dashboards/dashboard.json | yq -o=json ".dashboards"  > /grafana_dashboards/dashboard.json
 echo "Starting Grafana"
 /run.sh
